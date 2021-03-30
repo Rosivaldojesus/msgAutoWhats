fb0b62173130c97579fb1a53f99c632e1e35083e
Rosivaldojesus <js.rosivaldo@gmail.com>
2021-03-30 14:09:36 -0300

    Primeiro Código

---
+++ whats.py
@@ -0,0 +1,10 @@
+#Importando a biblioteca que executa a função
+#de envio de mensagem automatica do whtasapp
+import pywhatkit as kit
+
+#nomeDaBiblioteca.Função("Número que vai receber a msn",
+#   "Mensagem que você quer enviar",
+#       horário que você que enviar a mensagem")
+kit.sendwhatmsg("+55DD+número", "Mensagem desejada ", 13,36)
+
+
